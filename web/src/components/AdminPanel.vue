bbd68b95759aee142a518a1616f62ce9b83ff978
Paper-Dragon <2678885646@qq.com>
2026-01-16 12:48:39 +0800

    更新 AdminPanel.vue 中的分页设置，将页面大小更改为 [50, 80, 100]，并将默认页面大小调整为 50，以改善用户体验。

--- web/src/components/AdminPanel.vue
+++ web/src/components/AdminPanel.vue
@@ -156,7 +156,7 @@
           <el-pagination
             v-model:current-page="currentPage"
             v-model:page-size="pageSize"
-            :page-sizes="[10, 30, 50]"
+            :page-sizes="[50, 80, 100]"
             :total="total"
             layout="total, sizes, prev, pager, next"
             @size-change="loadDevices"
@@ -215,7 +215,7 @@ const changingPassword = ref(false)
 const passwordFormRef = ref(null)
 const passwordForm = ref({ oldPassword: '', newPassword: '', confirmPassword: '' })
 const currentPage = ref(1)
-const pageSize = ref(10)
+const pageSize = ref(50)
 const total = ref(0)
 let refreshTimer = null
 
